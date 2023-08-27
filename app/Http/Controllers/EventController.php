3b538d6c8c19e9ad5bab5999715b63c766f52af3
abdelrahmanna <abdoayman@icloud.com>
2023-08-27 08:23:58 +0300

    created event controller

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($perPage = 100, $page = 1)
+    {
+        /**
+         * Note: that we are using eager loading to load the eventable relationship due to the limited time.
+         * using joins and then fractal transformers would offer faster query time and the same results.
+         */
+        // get all events with their eventable models
+        $events = Event::query()
+            ->orderBy("created_at")
+            ->with("eventable")
+            ->paginate($perPage, ["*"], "page", $page);
+        return response()->json($events);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Event $event)
+    {
+        // get all request data
+        $data = $request->all();
+
+        // update event
+        $event->update($data);
+
+        // return updated event
+        return response()->json($event);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
